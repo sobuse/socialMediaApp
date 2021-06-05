3ead44c7a8e21b443d235b33ca2438408cceb88f
sammy <ehuerikenbaba@gmail.com>
2021-06-05 01:12:41 +0100

    create the server 2

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const app=require('express')();
-const server=require('http').server(app);
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
-const nextApp=next({dev})
-const handle = app.getRequestHandler();
-require('dotenv').config({path:"./config.env"})
-const connectDb=require('./utilsServer/connectDb')
-const PORT=process.env.PORT || 3000
-connectDb()
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+require("dotenv").config({ path: "./config.env" });
+const connectDb = require("./utilsServer/connectDb");
+const PORT = process.env.PORT || 3000;
+app.use(express.json()); // this is the body parser
+connectDb();
 
-nextApp.prepare().then(()=>{
-    // app.all('/api/auth',require('./api/auth'))
+nextApp.prepare().then(() => {
+  // app.all('/api/auth',require('./api/auth'))
 
-    app.all('*',(req,res)=>handle(req, res));
+  app.all("*", (req, res) => handle(req, res));
 
-    server.listen(PORT,err=>{
-        if(err)throw err;
-        console.log('express server running on ${PORT}')
-    })
-})
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log("express server running on ${PORT}");
+  });
+});
