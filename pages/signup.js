dfc485859f6005e7622c438949416d47828e986d
sammy <ehuerikenbaba@gmail.com>
2021-06-06 22:50:30 +0100

    format files

--- pages/signup.js
+++ pages/signup.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-function signup(){
-    return ( 
-        <div>
-            sign Up
-        </div>
-    )
+function signup() {
+  return <div>sign Up</div>;
 }
 
 export default signup;
